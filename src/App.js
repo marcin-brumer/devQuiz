1f5c2214106944761570446799a2403ee9a494be
Marcin Brumer <brumek31@gmail.com>
2019-01-13 11:11:09 +0100

    Added Game container and some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
+import Game from "./containers/Game/Game";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header" />
+        <header className="App-header">devQuiz</header>
+        <main className="App-main">
+          <Game />
+        </main>
       </div>
     );
   }
---
+++ src/containers/Game/Game.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styles from "./Game.module.css";
+
+class Quiz extends Component {
+  render() {
+    return <div className={styles.Game} />;
+  }
+}
+
+export default Quiz;
