e5e7c871906e2848fd0b021ba5f3414afda26554
Marcin Brumer <brumek31@gmail.com>
2019-01-16 17:46:26 +0100

    Added disable option for start button

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -2,18 +2,32 @@ import React from "react";
 import Technologies from "./Technologies/Technologies";
 import styles from "./WelcomeScreen.module.css";
 
-const welcomeScreen = props => (
-  <>
-    <h1>Welcome to devQuiz</h1>
-    <h3>First select the technologies from which you want to make a quiz</h3>
-    <Technologies
-      technologies={props.technologies}
-      technologyChecked={props.technologyChecked}
-    />
-    <button className={styles.StartBtn} onClick={props.gameStarted}>
-      Start
-    </button>
-  </>
-);
+const welcomeScreen = props => {
+  const techValues = Object.values(props.technologies);
+  let gameReady = false;
+  techValues.forEach(val => {
+    if (val === true) {
+      gameReady = true;
+    }
+  });
+
+  return (
+    <>
+      <h1>Welcome to devQuiz</h1>
+      <h3>First select the technologies from which you want to make a quiz</h3>
+      <Technologies
+        technologies={props.technologies}
+        technologyChecked={props.technologyChecked}
+      />
+      <button
+        className={styles.StartBtn}
+        onClick={props.gameStarted}
+        disabled={!gameReady}
+      >
+        Start
+      </button>
+    </>
+  );
+};
 
 export default welcomeScreen;
