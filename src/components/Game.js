867f9bd347ec93a36cd721d576a8a4dcad213cd8
Marcin Brumer <brumek31@gmail.com>
2019-04-03 16:54:27 +0200

    Switched from class component to functional component

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import { CSSTransitionGroup } from "react-transition-group";
 import { gamePhase } from "../constants";
@@ -6,38 +6,38 @@ import WelcomeScreen from "./WelcomeScreen/WelcomeScreen";
 import ResultScreen from "./ResultScreen/ResultScreen";
 import Quiz from "./Quiz/Quiz";
 
-class Game extends Component {
-  renderGame = () => {
-    const phase = this.props.phase;
-    switch (phase) {
-      case gamePhase.WELCOME_SCREEN:
-        return <WelcomeScreen />;
-      case gamePhase.QUIZ_SCREEN:
-        return <Quiz />;
-      case gamePhase.RESULT_SCREEN:
-        return <ResultScreen />;
-      default:
-        return null;
-    }
-  };
+const game = props => {
+  let gameScreen;
 
-  render() {
-    return (
-      <CSSTransitionGroup
-        component="div"
-        transitionName="fade"
-        transitionEnterTimeout={800}
-        transitionLeaveTimeout={500}
-        transitionAppear
-        transitionAppearTimeout={500}>
-        <div key={this.props.phase}>{this.renderGame()}</div>
-      </CSSTransitionGroup>
-    );
+  switch (props.phase) {
+    case gamePhase.WELCOME_SCREEN:
+      gameScreen = <WelcomeScreen />;
+      break;
+    case gamePhase.QUIZ_SCREEN:
+      gameScreen = <Quiz />;
+      break;
+    case gamePhase.RESULT_SCREEN:
+      gameScreen = <ResultScreen />;
+      break;
+    default:
+      gameScreen = null;
   }
-}
+
+  return (
+    <CSSTransitionGroup
+      component="div"
+      transitionName="fade"
+      transitionEnterTimeout={800}
+      transitionLeaveTimeout={500}
+      transitionAppear
+      transitionAppearTimeout={500}>
+      <div key={props.phase}>{gameScreen}</div>
+    </CSSTransitionGroup>
+  );
+};
 
 const mapStateToProps = state => ({
   phase: state.phase
 });
 
-export default connect(mapStateToProps)(Game);
+export default connect(mapStateToProps)(game);
