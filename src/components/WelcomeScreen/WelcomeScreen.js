55d734c88bb75be2caf5e3368ea65001c2015a8a
Marcin Brumer <brumek31@gmail.com>
2019-04-04 15:47:38 +0200

    Switched from class component to functional component

--- src/components/WelcomeScreen/WelcomeScreen.js
+++ src/components/WelcomeScreen/WelcomeScreen.js
@@ -1,43 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions/actions";
 import Technologies from "./Technologies/Technologies";
 import styles from "./WelcomeScreen.module.css";
 import QuestionsSlider from "./QuestionsSlider/QuestionsSlider";
 
-class WelcomeScreen extends Component {
-  render() {
-    const techValues = Object.values(this.props.technologies);
-    let gameReady = false;
-    techValues.forEach(val => {
-      if (val === true) {
-        gameReady = true;
-      }
-    });
-    return (
-      <>
-        <h1>Welcome to devQuiz</h1>
-        <h3>
-          First select the technologies from which you want to make a quiz
-        </h3>
-        <Technologies />
-        <h3>and decide how many questions you want to answer</h3>
-        <QuestionsSlider />
-        <button
-          className={styles.StartBtn}
-          onClick={() =>
-            this.props.startGame(
-              this.props.totalQuestions,
-              this.props.technologies
-            )
-          }
-          disabled={!gameReady}>
-          Start
-        </button>
-      </>
-    );
-  }
-}
+const welcomeScreen = props => {
+  const techValues = Object.values(props.technologies);
+  let gameReady = false;
+  techValues.forEach(val => {
+    if (val === true) {
+      gameReady = true;
+    }
+  });
+  return (
+    <>
+      <h1>Welcome to devQuiz</h1>
+      <h3>First select the technologies from which you want to make a quiz</h3>
+      <Technologies />
+      <h3>and decide how many questions you want to answer</h3>
+      <QuestionsSlider />
+      <button
+        className={styles.StartBtn}
+        onClick={() =>
+          props.startGame(props.totalQuestions, props.technologies)
+        }
+        disabled={!gameReady}>
+        Start
+      </button>
+    </>
+  );
+};
 
 const mapStateToProps = state => ({
   technologies: state.technologies,
@@ -52,4 +45,4 @@ const mapDispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   mapDispatchToProps
-)(WelcomeScreen);
+)(welcomeScreen);
